32d13fd9682895465b602a7c83c55ceaf7d3fd8a
Calvin McDowell <calvinmac40@hotmail.com>
2019-08-27 23:46:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,29 @@
 class Dog
   def initialize(name)
-    
+    @name = name
+  end
+  
+  def dog_name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def initialize(breed)
+    @breed = breed
+  end
+  
+  def dog_breed=(breed)
+    @breed = breed
+  end
+  
+  def dog_breed(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end
 end
