27303ba427b5eae7a7249be149ffc01fed5c5106
Calvin McDowell <calvinmac40@hotmail.com>
2019-08-27 23:50:16 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+# class Person
+#   def initialize(name)
+#     @name = name
+#   end
+  
+#   def name=(name)
+#     @name = name
+#   end
+  
+#   def name
+#     @name
+#   end
+# end
+
+class Dog 
+  attr_accessor :name, :breed 
+end
+
+    
