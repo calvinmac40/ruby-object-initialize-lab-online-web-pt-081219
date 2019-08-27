7506f5877612ac24f3220293d1c7b0fefb582a68
Calvin McDowell <calvinmac40@hotmail.com>
2019-08-27 23:37:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,2 @@
-class Dog
-  self.define_properties(props)
-  props.each do |prop|
-    define_method(prop) do
-      instance_variable_get(:"#{prop}", arg)
-    end
-  end
-end
-self_define_properties([:name,:breed])
+class Dog 
+  
